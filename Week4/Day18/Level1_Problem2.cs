741361f30328693d0cce90de2b9f8c8dee0f312c
srijachitturi <139261744+srijachitturi@users.noreply.github.com>
2026-03-11 16:28:55 +0530

    Add files via upload

---
+++ Week4/Day18/Level1_Problem2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter First Number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Second Number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Operator [ + - * / ]: ");
+        char op = char.Parse(Console.ReadLine());
+
+        double result = 0;
+
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Error: Cannot divide by zero.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Invalid Operation");
+                return;
+        }
+
+        Console.WriteLine("Result: " + result);
+        Console.ReadLine();
+    }
+}
