13187ce43a186de943e07112336dd2ee56863749
srijachitturi <139261744+srijachitturi@users.noreply.github.com>
2026-03-11 17:31:19 +0530

    Add files via upload

---
+++ Week4/Day18/Level2_Problem2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Level2Problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int evenCount = 0;
+            int oddCount = 0;
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+        
+                sum += i;
+
+                if (i % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine($"Even Count: {evenCount}");
+            Console.WriteLine($"Odd Count: {oddCount}");
+            Console.WriteLine($"Sum: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
