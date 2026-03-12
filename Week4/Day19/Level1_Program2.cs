6a5c94f847a0983205575263e0441b8e2a4ea9da
srijachitturi <139261744+srijachitturi@users.noreply.github.com>
2026-03-12 16:35:12 +0530

    Add files via upload

---
+++ Week4/Day19/Level1_Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Student
+    {
+        public double CalculateAverage(int m1, int m2, int m3)
+        {
+            return (m1 + m2 + m3) / 3.0;
+        }
+
+        public string GetGrade(double average)
+        {
+            if (average >= 80) return "A";
+            else if (average >= 70) return "B";
+            else return "C";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+
+            double avg = stu.CalculateAverage(80, 70, 90);
+            string grade = stu.GetGrade(avg);
+
+            Console.WriteLine($"Average = {avg}, Grade = {grade}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week4/Day19/Problem1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Product
+    {
+        // Private variables
+        private int _productId;
+        private string _productName;
+        private double _unitPrice;
+        private int _qty;
+
+        // Constructor allowing productId as parameter
+        public Product(int productId)
+        {
+            this._productId = productId;
+            this._productName = string.Empty;
+            this._unitPrice = 0.0;
+            this._qty = 0;
+        }
+
+        // ProductId – readonly property
+        public int ProductId
+        {
+            get { return this._productId; }
+        }
+
+        // Properties for all private variables
+        public string ProductName
+        {
+            get { return this._productName; }
+            set { this._productName = value; }
+        }
+
+        public double UnitPrice
+        {
+            get { return this._unitPrice; }
+            set { this._unitPrice = value; }
+        }
+
+        public int Quantity
+        {
+            get { return this._qty; }
+            set { this._qty = value; }
+        }
+        public void ShowDetails()
+        {
+            double totalAmount = _unitPrice * _qty;
+            Console.WriteLine($"Product ID  : {ProductId}");
+            Console.WriteLine($"Product Name: {ProductName}");
+            Console.WriteLine($"Unit Price  : {UnitPrice}");
+            Console.WriteLine($"Quantity    : {Quantity}");
+            Console.WriteLine($"Total Amount: {totalAmount}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product p1 = new Product(501);
+            p1.ProductName = "Mechanical Keyboard";
+            p1.UnitPrice = 2500.00;
+            p1.Quantity = 3;
+
+            p1.ShowDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
