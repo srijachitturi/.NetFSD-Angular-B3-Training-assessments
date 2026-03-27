bfb1c1696022063d1582a8b54894f7bbc036a5bc
srijachitturi <139261744+srijachitturi@users.noreply.github.com>
2026-03-27 09:59:45 +0530

    Add files via upload

---
+++ week6/Day28/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Pid { get; set; }
+        public string Pname { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+    }
+}
---
+++ week6/Day28/controller/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product { Pid = 1, Pname = "Laptop", Price = 75000, Qty = 5 },
+            new Product { Pid = 2, Pname = "Mobile", Price = 30000, Qty = 10 },
+            new Product { Pid = 3, Pname = "Tablet", Price = 25000, Qty = 7 },
+            new Product { Pid = 4, Pname = "Smart Watch", Price = 12000, Qty = 12 },
+            new Product { Pid = 5, Pname = "Headphones", Price = 2000, Qty = 20 },
+           
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var p = products.FirstOrDefault(x => x.Pid == id);
+            return View(p);
+        }
+    }
+}
